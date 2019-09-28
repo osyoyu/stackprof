10366f69754d4d0b5e8365cc173d9b409640d5c2
osyoyu <osyoyu@osyoyu.com>
2019-09-28 13:12:50 +0900

    Implement merging of duplicate callstacks coming from different dumps

--- lib/stackprof/dump_reader.rb
+++ lib/stackprof/dump_reader.rb
@@ -30,6 +30,10 @@ module StackProf
       raise ArgumentError, "cannot combine different versions of dumps" if reports.map {|r| r[:version]}.uniq.size > 1
       raise ArgumentError, "cannot combine different modes" if reports.map {|r| r[:mode]}.uniq.size > 1
 
+      concatenated_raw = reports.map {|r| r[:raw]}.concat.flatten.compact
+      # raw = merge_duplicate_raw_segments(concatenated_raw)
+      raw = concatenated_raw
+
       data = {
         version: reports[0][:version],
         mode: reports[0][:mode],
@@ -38,11 +42,41 @@ module StackProf
         gc_samples: reports.map {|r| r[:gc_samples]}.sum,
         missed_samples: reports.map {|r| r[:missed_samples]}.sum,
         frames: reports.map {|r| r[:frames]}.inject(&:merge),
-        raw: reports.map {|r| r[:raw]}.concat.flatten.compact,
-        raw_timestamp_deltas: reports.map {|r| r[:raw_timestamp_deltas]}.concat.flatten.compact,
+        raw: raw,
+        raw_timestamp_deltas: reports.map {|r| r[:raw_timestamp_deltas]}.concat.flatten.compact,  # is this correct?
       }
 
       data
+    end
+
+    def merge_duplicate_raw_segments(raw_array)
+      stacks = {}
+      while len = raw_array.shift
+        callstack = raw_array.slice!(0, len)
+        appearance_count = raw_array.shift
+        hashcode = callstack.hash
+
+        if !stacks.key?(hashcode)
+          stacks[hashcode] = {
+            callstack: callstack,
+            appearance_count: appearance_count
+          }
+        else
+          # merge
+          stacks[hashcode][:appearance_count] += appearance_count
+        end
+      end
+
+      # flatten to the original format
+      merged_raw = stacks.map do |hashcode, stack|
+        r = []
+        r << stack[:callstack].size
+        r.append(stack[:callstack])
+        r << stack[:appearance_count]
+        r
+      end.flatten
+
+      merged_raw
     end
   end
 end
