1bdca77d973039e795685879429bd462272e1af7
osyoyu <osyoyu@osyoyu.com>
2019-09-28 17:59:37 +0900

    Use DumpMerger from bin/stackprof

--- lib/stackprof.rb
+++ lib/stackprof.rb
@@ -1,4 +1,5 @@
 require "stackprof/stackprof"
 
 StackProf.autoload :Report, "stackprof/report.rb"
+StackProf.autoload :DumpMerger, "stackprof/dump_merger.rb"
 StackProf.autoload :Middleware, "stackprof/middleware.rb"
