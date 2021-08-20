98460a8e6df41b867f1539f5d124f2343b146315
osyoyu <osyoyu@osyoyu.com>
2021-08-20 18:15:33 +0900

    Don't rely on iseq_ptr when merging dumps from multiple processes

--- lib/stackprof/dump_merger.rb
+++ lib/stackprof/dump_merger.rb
@@ -13,12 +13,11 @@ module StackProf
       raise ArgumentError, "cannot combine different versions of dumps" if dumps.map {|r| r[:version]}.uniq.size > 1
       raise ArgumentError, "cannot combine different modes" if dumps.map {|r| r[:mode]}.uniq.size > 1
 
-      frames = dumps.map {|r| r[:frames]}.inject(&:merge)
-      raws = dumps.map {|r| r[:raw]}.concat.flatten.compact
-      if aggressive
-        frames, raws = aggressive_merge(frames, raws)
-      end
-      raws = merge_duplicate_raw_segments(raws)
+      # (frame.merge!({ iseq_ptr: Integer }))[]
+      frames_master_by_name = {}
+
+      raws = dumps.map {|d| aggressively_renamed_raws(frames_master_by_name, d[:frames], d[:raw] || [])}.flatten
+      frames = convert_frames_to_hash(frames_master_by_name)
 
       data = {
         version: dumps[0][:version],
@@ -29,7 +28,7 @@ module StackProf
         missed_samples: dumps.map {|r| r[:missed_samples]}.sum,
         frames: frames,
         raw: raws,
-        # raw_timestamp_deltas: dumps.map {|r| r[:raw_timestamp_deltas]}.concat.flatten.compact,  # is this correct?
+        raw_timestamp_deltas: dumps.map {|r| r[:raw_timestamp_deltas]}.concat.flatten.compact,  # is this correct?
       }
 
       data
@@ -106,6 +105,60 @@ module StackProf
       end
 
       [frames, res_raw.flatten]
+    end
+
+    # Merge dumps from multiple processes, without fear of bugs.
+    #
+    # @param [Hash] frames_master Canonicalized frames
+    # @param [Hash] frames_single Frames from a single dump
+    # @param [Array<Number>] raws_single Raws from a single dump
+    # @return
+    def aggressively_renamed_raws(frames_master_by_name, frames_single, raws_single)
+      frames = convert_frames_to_array(frames_single)
+
+      # TODO: Kuso code; this block shouldn't be here
+      frames.each do |frame|
+        frame_name = normalized_frame_name(frame)
+        if !frames_master_by_name.key?(frame_name)
+          # first time, record
+          frames_master_by_name[frame_name] = frame
+        else
+          # Merge `samples` count and `total_samples` count
+          frames_master_by_name[frame_name][:samples] += frame[:samples]
+          frames_master_by_name[frame_name][:total_samples] += frame[:total_samples]
+        end
+      end
+
+      res_raw = []
+      while len = raws_single.shift
+        res_raw << len
+        callstack = raws_single.slice!(0, len)
+        res_raw << callstack.map do |iseq_ptr|
+          # Rewrite iseq_ptr based on name
+          frame_name = normalized_frame_name(frames_single[iseq_ptr])
+          frames_master_by_name[frame_name][:iseq_ptr]
+        end
+        res_raw << raws_single.shift
+      end
+
+      res_raw
+    end
+
+    # { [iseq_ptr: Number] => Hash } to Hash[]
+    def convert_frames_to_array(frames)
+      frames.map do |iseq_ptr, frame|
+        frame.merge!({ iseq_ptr: iseq_ptr })
+      end
+    end
+
+    # Hash[] to { [iseq_ptr: Number] => Hash }
+    def convert_frames_to_hash(frames)
+      f = frames.map {|frame_name, frame|
+        f = frame.dup
+        iseq_ptr = f.delete(:iseq_ptr)
+        [iseq_ptr, f]
+      }
+      f.to_h
     end
   end
 end
